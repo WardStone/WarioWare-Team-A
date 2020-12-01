1c85fa7e331d7b8c549c8f3f51c0765f2b384740
Paul PARET <p.paret@rubika-edu.com>
2020-12-01 18:48:42 +0100

    Core Gameplay + Lvl Elements + Goal

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioACommAkuma/MiniGame3/AssetMiniGame3/ScriptMiniGame3/GoalManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioName
+{
+    namespace MiniGameName
+    {
+        /// <summary>
+        /// Paul PARET
+        /// </summary>
+        public class GoalManager : TimedBehaviour
+        {
+
+            [HideInInspector] public bool asWin = false;
+
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+
+                if (Tick == 8)
+                {
+                    if (asWin)
+                    {
+                        //Manager.Instance.Result(true);
+                    }
+                    else
+                    {
+                        //Manager.Instance.Result(false);
+                    }
+                }
+
+            }
+
+
+            private void OnTriggerEnter2D(Collider2D other)
+            {
+                if (other.gameObject.tag == "Player")
+                {
+                    asWin = true;
+                    Debug.Log("You Win");
+                }
+            }
+        }
+    }
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioACommAkuma/MiniGame3/AssetMiniGame3/ScriptMiniGame3/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrioName
+{
+    namespace MiniGameName
+    {
+
+        /// <summary>
+        /// Paul PARET
+        /// </summary>
+        public class PlayerController : TimedBehaviour
+        {
+            [Header("Player Movement")]
+            public float playerTorqueScale;
+            [Range(0f, 10f)] public float playerDragSlow;
+            [Range(0f, 10f)] public float playerDragQuick;
+
+            [Header("TickEvent")]
+            public float impulseScale;
+
+            [Header("Debug")]
+            public float playerTorque;
+
+            private float rBumperHold = 0f;
+            private float lBumperHold = 0f;
+            private Rigidbody2D playerRb;
+
+            public override void Start()
+            {
+                base.Start(); //Do not erase this line!
+
+                playerRb = GetComponent<Rigidbody2D>();
+                playerTorque = 0f;
+
+            }
+
+            //FixedUpdate is called on a fixed time.
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate(); //Do not erase this line!
+
+                
+            }
+
+            private void Update()
+            {
+                GetInput();
+            }
+
+            //TimedUpdate is called once every tick.
+            public override void TimedUpdate()
+            {
+                if (Tick < 8)
+                {
+                    ApplyImpule();
+                }
+            }
+
+
+            private void GetInput()
+            {
+                rBumperHold = Input.GetAxis("Right_Trigger");
+
+                lBumperHold = Input.GetAxis("Left_Trigger");
+
+                DirManager();
+                SpeedManager();
+                ApplyTorque();
+            }
+
+            private void DirManager()
+            {
+                if (lBumperHold > 0 && rBumperHold <= 0)
+                    playerTorque = -1f;
+
+                else if (lBumperHold <= 0 && rBumperHold > 0)
+                    playerTorque = 1f;
+
+                else if ((lBumperHold > 0 && rBumperHold > 0) || (lBumperHold <= 0 && rBumperHold <= 0))
+                    playerTorque = 0f;
+            }
+
+            /// <summary>
+            /// Set the boat drag to increase and decrease the speed
+            /// </summary>
+            private void SpeedManager()
+            {
+                if (lBumperHold > 0 && rBumperHold > 0)
+                    playerRb.drag = playerDragQuick;
+                else
+                    playerRb.drag = playerDragSlow;
+            }
+
+            private void ApplyTorque()
+            {
+                playerRb.AddTorque(playerTorque * playerTorqueScale, ForceMode2D.Force);
+            }
+
+            /// <summary>
+            /// Give the player an impulse forward (USE IN TIMED UPDATE)
+            /// </summary>
+            private void ApplyImpule()
+            {
+                playerRb.AddForce(transform.TransformDirection(Vector2.right) * impulseScale, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
