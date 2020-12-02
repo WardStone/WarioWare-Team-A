9072ab5fc8932da6569dd547df3f87a41ee277d7
Bastien <b.prigent@rubika-edu.com>
2020-12-02 11:50:58 +0100

    Add new packages

--- WarioWare/Assets/Setup/Scripts/Editor/IDCardEditor.cs
+++ WarioWare/Assets/Setup/Scripts/Editor/IDCardEditor.cs
@@ -49,7 +49,8 @@ public class IDCardEditor : Editor {
 		
 		EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(idCard.microGameScene)));
 
-
+		EditorGUILayout.Space(20);
+		if(idCard.microGameScene.BuildIndex == -1)
 		if (GUILayout.Button("Add To Build")) { AddScene(idCard.microGameScene.EditorSceneAsset); }
 
 		EditorUtility.SetDirty(idCard);
