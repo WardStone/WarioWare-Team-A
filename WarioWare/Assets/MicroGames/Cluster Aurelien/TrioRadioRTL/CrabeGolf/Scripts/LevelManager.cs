47dbb59165002e7af2adf711d2a98d34f49d96f9
Paul PARET <p.paret@rubika-edu.com>
2020-12-07 18:04:29 +0100

    Playtest Package Added

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RadioRTL
+{
+    /// <summary>
+    /// Théo Valet
+    /// </summary>
+}
+
+public class LevelManager : TimedBehaviour
+{
+
+    /*
+        public override void Start ()
+        {
+            base.Start();
+
+            switch (currentDifficulty)
+            {
+                case Manager.Difficulty.EASY:
+                    //insérer la logique quand le jeu est facile
+                    break;
+                case Manager.Difficulty.MEDIUM:
+                    //insérer la logique quand le jeu est facile
+                    break;
+                case Manager.Difficulty.HARD:
+                    //insérer la logique quand le jeu est facile
+                    break;
+            }
+        }
+    */
+}
---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioRadioRTL/CrabeGolf/Scripts/PlayerBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RadioRTL
+{
+    /// <summary>
+    /// Théo Valet
+    /// </summary>
+}
+public class PlayerBehavior : MonoBehaviour
+{
+    public bool StrikeUnlock = false;
+    public GameObject NormalCrab;
+
+
+    void Start()
+    {
+
+        if (Input.GetButtonDown("A_Button"))
+        {
+            StrikeUnlock = true;
+            Frappe();
+        }
+    }
+
+    void Frappe()
+    {
+        if (Input.GetButtonUp("A_Button") && StrikeUnlock)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
