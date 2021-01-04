ab40fe991f591e7a548ab4f90f8836fc1dc8650e
Paul PARET <p.paret@rubika-edu.com>
2021-01-04 14:48:15 +0100

    Liquid Trail In Progress

---
+++ WarioWare/Assets/MicroGames/Cluster Aurelien/TrioACommAkuma/PirateGrog/AssetPirateGrog/ScriptPirateGrog/LiquidCollisionDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ACommeAkuma
+{
+    namespace PirateGrog
+    {
+        /// <summary>
+        /// PARET Paul
+        /// </summary>
+        public class LiquidCollisionDetection : MonoBehaviour
+        {
+            [HideInInspector] public bool asCollided = false;
+
+            private void OnCollisionEnter2D(Collision2D other)
+            {
+                asCollided = true;
+            }
+        }
+    }
+}
+
